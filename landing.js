8ebbcc96205185991ec03c27690c672c52dc1b19
autosam <autosam.sm@gmail.com>
2025-02-19 23:21:58 +0330

    add feedback and gift code section

--- landing.js
+++ landing.js
@@ -1,33 +1,86 @@
 (async () => {
-    const getPosts = async () => {
-        const res = await fetch('assets/posts.json');
-        const posts = res.json();
-        return posts;
-    }
+  const getPosts = async () => {
+    const res = await fetch("assets/posts.json");
+    const posts = res.json();
+    return posts;
+  };
 
-    const createPostElements = (posts) => {
-        const postsContainer = document.querySelector('.posts');
-        const cloneable = document.querySelector('.cloneable > .postContainer');
-        posts.forEach(post => {
-            const component = cloneable.cloneNode(true);
-            const image = component.querySelector('.postContainer__imageContainer > img');
-            const subTitle = component.querySelector('.postContainer__infoContainer__subTitle');
-            const title = component.querySelector('.postContainer__infoContainer__title');
+  const createPostElements = (posts) => {
+    const postsContainer = document.querySelector(".posts");
+    const cloneable = document.querySelector(".cloneable > .postContainer");
+    posts.forEach((post) => {
+      const component = cloneable.cloneNode(true);
+      const image = component.querySelector(
+        ".postContainer__imageContainer > img"
+      );
+      const subTitle = component.querySelector(
+        ".postContainer__infoContainer__subTitle"
+      );
+      const title = component.querySelector(
+        ".postContainer__infoContainer__title"
+      );
 
-            image.src = `assets/post-images/${post.image ?? 'lg.png'}`;
-            subTitle.textContent = post.subTitle;
-            title.textContent = post.title;
+      image.src = `assets/post-images/${post.image ?? "lg.png"}`;
+      subTitle.textContent = post.subTitle;
+      title.textContent = post.title;
 
-            component.onclick = () => {
-                window.location.href = (`blog?post=${post.title}`);
-            }
+      component.onclick = () => {
+        window.location.href = `blog?post=${post.title}`;
+      };
 
-            postsContainer.appendChild(component)
-        })
-    }
+      postsContainer.appendChild(component);
+    });
+  };
 
-    const posts = await getPosts();
-    createPostElements(posts);
+  const initializeFeedback = () => {
+    const FORM_URL =
+      "https://docs.google.com/forms/d/e/1FAIpQLSenonpIhjHL8BYJbnOHqF2KudJiDciEveJG56BdGsvJ01-rTA/formResponse?usp=pp_url&entry.1753365981=%%UID%%&entry.233513152=%%TEXT%%";
+
+    const [
+      usernameInput,
+      textInput,
+      submitBtn,
+      feedbackInProgressMessage,
+      feedbackDoneMessage,
+    ] = document.querySelectorAll(
+      "#feedback__username, #feedback__text, #feedback__submit, #feedback__in-progress, #feedback__done"
+    );
+
+    textInput.oninput = (event) => {
+      const value = event.target.value?.trim();
+      submitBtn.disabled = !value;
+    };
+
+    submitBtn.onclick = () => {
+      const url = FORM_URL.replace(
+        "%%UID%%",
+        usernameInput.value?.trim()
+      ).replace("%%TEXT%%", textInput.value?.trim());
+
+      textInput.disabled = true;
+      usernameInput.disabled = true;
+      submitBtn.disabled = true;
+      feedbackDoneMessage.classList.add("hidden");
+      feedbackInProgressMessage.classList.remove("hidden");
+
+      fetch(url)
+        .catch((e) => {})
+        .finally(() => {
+          setTimeout(() => {
+            textInput.disabled = false;
+            usernameInput.disabled = false;
+
+            textInput.value = "";
+            usernameInput.value = "";
+
+            feedbackDoneMessage.classList.remove("hidden");
+            feedbackInProgressMessage.classList.add("hidden");
+          }, 1000 + (Math.random() * 3000));
+        });
+    };
+  };
+
+  initializeFeedback();
+  const posts = await getPosts();
+  createPostElements(posts);
 })();
-
-
