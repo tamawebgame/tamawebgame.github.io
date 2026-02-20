08ba4f45d11f00601c2b40a85a2dcf7444a9e652
autosam <autosam.sm@gmail.com>
2026-02-20 22:58:28 +0330

    fix: prod build

--- creator/webpack.config.js
+++ creator/webpack.config.js
@@ -1,64 +1,79 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode !== "production";
 
-module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
-  entry: './app.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: [
-        '@babel/preset-env',
-        ['@babel/preset-react', {
-          'runtime': 'automatic'
-        }]
+  return {
+    mode: isDevelopment ? "development" : "production",
+
+    entry: "./app.jsx",
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+              ],
+              plugins: [
+                isDevelopment && require.resolve("react-refresh/babel"),
+              ].filter(Boolean),
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
       ],
-      plugins: [
-        isDevelopment && require.resolve('react-refresh/babel'),
-      ].filter(Boolean),
     },
-  },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    static: false,
-    port: 3000,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-    client: {
-      overlay: true,
-      progress: true,
+
+    resolve: {
+      extensions: [".js", ".jsx"],
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
-    isDevelopment && new webpack.HotModuleReplacementPlugin(),
-    isDevelopment && new ReactRefreshWebpackPlugin(),
-  ].filter(Boolean),
+
+    devServer: {
+      static: false,
+      port: 3000,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+      client: {
+        overlay: true,
+        progress: true,
+      },
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+      }),
+
+      // Properly define NODE_ENV for React
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(
+          isDevelopment ? "development" : "production"
+        ),
+      }),
+
+      // Dev-only plugins
+      isDevelopment && new webpack.HotModuleReplacementPlugin(),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
+  };
 };
